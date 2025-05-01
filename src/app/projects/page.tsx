9a713cc030dd61fddee36512d919c98d3ac6e8e2
Muhammad Umer <mhadisb1212@gmail.com>
2025-05-01 14:31:23 +0500

    removal: removed cn util correction: projects data updated from new projects array

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,8 @@
 import Projects from '@/components/projects'
-import { getProjects } from '@/libs/projects'
 import React from 'react'
+import { projects } from '@/content/projects'
 
 export default async function ProjectsPage() {
-  const projects = await getProjects()
   return (
     <div className='space-y-4'>
       <h1 className='font-sourceCodePro text-xl font-bold'>Projects</h1>
